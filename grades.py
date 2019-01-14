9ff6a2d3c1b915412cfa436bf4b746c2287c8d4b
dark <bentalesnik@gmail.com>
2019-01-14 14:33:02 -0500

    Update grades.py

--- grades.py
+++ grades.py
@@ -100,26 +100,6 @@ class schoologay:
                 print ("{0} has used the gradeset reset command in {1}".format(ctx.message.author.name, ctx.message.server.name))
                 await self.bot.say('This is a public channel. **Please DM me.**')
 
-#    @commands.command(pass_context=True) #Without Section Details
-#    async def grades(self, ctx):
-#        user = ctx.message.author
-#        sc = schoolopy.Schoology(schoolopy.Auth(self.settings[user.id]["KEY"], self.settings[user.id]["SECRET"]))
-#        sc.limit = 10  # Only retrieve 10 objects max
-#        data = sc.get_user_grades(sc.get_me().uid)
-#        embed=discord.Embed(title="",  description="**School:** {0}\n**Grad Year:** {1}".format(sc.get_school(sc.get_me().building_id).title, sc.get_me().grad_year), color=123123, timestamp=datetime.utcnow())
-#        embed.set_author(name="{}'s Info".format(sc.get_me().name_display), icon_url=dark_alt)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[0]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[0]['final_grade'][0]['grade'], data[0]['final_grade'][0]['grading_category'][0]['grade'], data[0]['final_grade'][0]['grading_category'][1]['grade'], data[0]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[1]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[1]['final_grade'][0]['grade'], data[1]['final_grade'][0]['grading_category'][0]['grade'], data[1]['final_grade'][0]['grading_category'][1]['grade'], data[1]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[2]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[2]['final_grade'][0]['grade'], data[2]['final_grade'][0]['grading_category'][0]['grade'], data[2]['final_grade'][0]['grading_category'][1]['grade'], data[2]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[3]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[3]['final_grade'][0]['grade'], data[3]['final_grade'][0]['grading_category'][0]['grade'], data[3]['final_grade'][0]['grading_category'][1]['grade'], data[3]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[4]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[4]['final_grade'][0]['grade'], data[4]['final_grade'][0]['grading_category'][0]['grade'], data[4]['final_grade'][0]['grading_category'][1]['grade'], data[4]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[5]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[5]['final_grade'][0]['grade'], data[5]['final_grade'][0]['grading_category'][0]['grade'], data[5]['final_grade'][0]['grading_category'][1]['grade'], data[5]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[6]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[6]['final_grade'][0]['grade'], data[6]['final_grade'][0]['grading_category'][0]['grade'], data[6]['final_grade'][0]['grading_category'][1]['grade'], data[6]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.add_field(name='{0} {1}'.format(sc.get_section(data[7]["section_id"]).course_title, sc.get_section(data[7]["section_id"]).section_title), value="**Final:** {0}\n**Minor:** {1}\n**Major:** {2}\n**Practice:** {3}".format(data[7]['final_grade'][0]['grade'], data[7]['final_grade'][0]['grading_category'][0]['grade'], data[7]['final_grade'][0]['grading_category'][1]['grade'], data[7]['final_grade'][0]['grading_category'][2]['grade']), inline=False)
-#        embed.set_thumbnail(url='{}'.format(sc.get_me().picture_url))
-#        embed.set_footer(text='+grades', icon_url=dark)
-#        await self.bot.say(embed=embed)
-
     @commands.command(pass_context=True, no_pm=False) # With Section Details
     async def grades(self, ctx):
         if ctx.message.channel.is_private:
